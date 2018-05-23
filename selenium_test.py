04c50ca08e6776a3fa2d4f1c952cc4eafa9c6c20
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-05-23 23:15:25 +0000

    Organize

--- selenium_test.py
+++ selenium_test.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://www.twitch.tv/videos/264031046")
